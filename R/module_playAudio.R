bca9dd97847de073195cae5d6a97892d3388d6f4
Thomas Sostarics <tsostarics@gmail.com>
2023-12-27 14:48:01 -0600

    Finish implementing stop button for #32

--- R/module_playAudio.R
+++ R/module_playAudio.R
@@ -1,18 +1,54 @@
 playAudioUI <- function(id) {
   ns <- NS(id)
 
-  shiny::actionButton(
-    inputId = ns("playVisibleFile"),
-    label = "Play File",
-    title = "Click to play file (if one is plotted & available)",
-    icon = shiny::icon("xmark")
+  tagList(
+    tags$head(
+      tags$style(
+        shiny::HTML("
+          .smallPlayButton {
+            width: 75%;
+
+          }
+          .bigPlayButton {
+          width: 100%
+          }
+          .hideStopButton {
+            display: none;
+          }
+          .showStopButton {
+            width: 20%;
+            padding-left: 0px;
+            padding-right: 0px;
+            border: none;
+            margin-left: 5.5px;
+            padding-top: .7em;
+            padding-bottom: .7em;
+          }"))
+    ),
+    span(style = "display:inline-block",
+         # HTML(
+    shiny::actionButton(
+      inputId = ns("playVisibleFile"),
+      label = "Play File",
+      title = "Click to play file (if one is plotted & available)",
+      icon = shiny::icon("xmark"),
+      class = "bigPlayButton"
+    ),
+    shiny::actionButton(
+      inputId = ns("stopButton"),
+      label = NULL,
+      title = "Click to stop playing file",
+      icon = shiny::icon("stop", style = "font-size:1.5em;"),
+      class = "hideStopButton"
+    )
+    )
   )
 }
 
 playAudioServer <- function(id, loadedFile, currentWave, destroyLoadedAudio,
                             audioInfo, nPlotted, plotSubset) {
   moduleServer(id, function(input, output, session) {
-
+    ns <- NS(id)
     observe({
       if (is.null(loadedFile$data) ||
           !nPlotted$is_one ||
@@ -29,6 +65,13 @@ playAudioServer <- function(id, loadedFile, currentWave, destroyLoadedAudio,
       }
     })
 
+    shiny::observeEvent(input$stopButton, {
+      if (is.null(currentWave$instance))
+        return(NULL)
+
+      audio::pause.audioInstance(currentWave$instance)
+      audio::rewind.audioInstance(currentWave$instance)
+    })
 
     shiny::observeEvent(input$playVisibleFile, {
       # This button is only active if we have data loaded, we've set the audio
@@ -48,11 +91,16 @@ playAudioServer <- function(id, loadedFile, currentWave, destroyLoadedAudio,
         message(paste0("File ", file_to_open, " not found."))
         shiny::updateActionButton(session, "playVisibleFile",icon = icon("xmark"))
         currentWave$exists <- FALSE
+
         return(NULL)
       }
 
       currentWave$exists <- TRUE
 
+      shinyjs::addClass(("playVisibleFile"), "smallPlayButton")
+      shinyjs::removeClass("playVisibleFile", "bigPlayButton")
+      shinyjs::addClass(("stopButton"), "showStopButton")
+      shinyjs::removeClass(("stopButton"), "hideStopButton")
       # If the file exists, then we need to load it as a wave object.
       # Once it's loaded, then it can be played, but we need to make sure to
       # close any running audio instances, otherwise (at best) the audio
