995ac6561b754cb14eed76c9707c863d24fc373c
Thomas Sostarics <tsostarics@gmail.com>
2024-01-17 16:56:21 -0600

    Move some of the theme options to the color module to cleanup the plot code a bit

--- R/module_color.R
+++ R/module_color.R
@@ -57,6 +57,7 @@ colorUI <- function(id) {
 #'
 #' @return A module server function
 #' @importFrom shiny NS
+#' @importFrom ggplot2 element_line element_text element_rect aes
 colorServer <- function(id,
                         plotSettings_ref,
                         updatePlot_reactive,
@@ -94,23 +95,25 @@ colorServer <- function(id,
 
         plotSettings_ref$themeColors <-
           ggplot2::theme(
-            panel.background = ggplot2::element_rect(fill = "#1d1f21"),
-            panel.grid.major = ggplot2::element_line(colour = "grey20"),
-            panel.grid.minor = ggplot2::element_line(colour = "grey20"),
-            axis.line = ggplot2::element_line(colour = "grey20"),
-            axis.text.x = ggplot2::element_text(colour = "white"),
-            axis.text.y = ggplot2::element_text(colour = "white"),
-            axis.title.x = ggplot2::element_text(colour = "white"),
-            axis.title.y = ggplot2::element_text(colour = "white"),
-            strip.background = ggplot2::element_rect(fill = "grey20"),
-            strip.text = ggplot2::element_text(colour = "white"),
-            legend.background = ggplot2::element_rect(fill = "grey20"),
-            legend.text = ggplot2::element_text(colour = "white"),
-            legend.title = ggplot2::element_text(colour = "white"),
-            plot.title = ggplot2::element_text(colour = "white"),
-            plot.subtitle = ggplot2::element_text(colour = "white"),
-            plot.caption = ggplot2::element_text(colour = "white"),
-            plot.background = ggplot2::element_rect(fill = "#1d1f21")
+            panel.background = element_rect(fill = "#1d1f21"),
+            panel.grid.major = element_line(colour = "grey20"),
+            panel.grid.minor = element_line(colour = "grey20"),
+            axis.line = element_line(colour = "grey20"),
+            axis.text.x = element_text(colour = "white"),
+            axis.text.y = element_text(colour = "white"),
+            axis.title.x = element_text(colour = "white"),
+            axis.title.y = element_text(colour = "white"),
+            strip.background = element_rect(fill = "grey20"),
+            strip.text = element_text(colour = "white"),
+            legend.background = element_rect(fill = "grey20"),
+            legend.text = element_text(colour = "white"),
+            legend.title = element_text(colour = "white"),
+            plot.title = element_text(colour = "white"),
+            plot.subtitle = element_text(colour = "white"),
+            plot.caption = element_text(colour = "white"),
+            plot.background = element_rect(fill = "#1d1f21"),
+            panel.grid = element_line(linewidth = .3),
+            legend.position = 'top',
           )
       } else{
         colourpicker::updateColourInput(session, "lineColor", value = "blue")
@@ -120,23 +123,25 @@ colorServer <- function(id,
 
         plotSettings_ref$themeColors <-
           ggplot2::theme(
-            panel.background = ggplot2::element_rect(fill = "white"),
-            panel.grid.major = ggplot2::element_line(colour = "grey80"),
-            panel.grid.minor = ggplot2::element_line(colour = "grey80"),
-            axis.line = ggplot2::element_line(colour = "grey80"),
-            axis.text.x = ggplot2::element_text(colour = "black"),
-            axis.text.y = ggplot2::element_text(colour = "black"),
-            axis.title.x = ggplot2::element_text(colour = "black"),
-            axis.title.y = ggplot2::element_text(colour = "black"),
-            strip.background = ggplot2::element_rect(fill = "grey80"),
-            strip.text = ggplot2::element_text(colour = "black"),
-            legend.background = ggplot2::element_rect(fill = "white"),
-            legend.text = ggplot2::element_text(colour = "black"),
-            legend.title = ggplot2::element_text(colour = "black"),
-            plot.title = ggplot2::element_text(colour = "black"),
-            plot.subtitle = ggplot2::element_text(colour = "black"),
-            plot.caption = ggplot2::element_text(colour = "black"),
-            plot.background = ggplot2::element_rect(fill = "white")
+            panel.background = element_rect(fill = "white"),
+            panel.grid.major = element_line(colour = "grey80"),
+            panel.grid.minor = element_line(colour = "grey80"),
+            axis.line = element_line(colour = "grey80"),
+            axis.text.x = element_text(colour = "black"),
+            axis.text.y = element_text(colour = "black"),
+            axis.title.x = element_text(colour = "black"),
+            axis.title.y = element_text(colour = "black"),
+            strip.background = element_rect(fill = "grey80"),
+            strip.text = element_text(colour = "black"),
+            legend.background = element_rect(fill = "white"),
+            legend.text = element_text(colour = "black"),
+            legend.title = element_text(colour = "black"),
+            plot.title = element_text(colour = "black"),
+            plot.subtitle = element_text(colour = "black"),
+            plot.caption = element_text(colour = "black"),
+            plot.background = element_rect(fill = "white"),
+            panel.grid = element_line(linewidth = .3),
+            legend.position = 'top'
           )
       }
       updatePlot_reactive()
